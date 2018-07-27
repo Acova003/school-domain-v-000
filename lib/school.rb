c52a655f98dea67fcacd5a325d2b406f1b3f1e90
Amee Covarrubias <ameecova@yahoo.com>
2018-07-27 23:40:33 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+  attr_accessor  :name, :roster
+  attr_reader :add_student, :grade
+  
+  def initialize(name)
+    @name = name
+    @roster = roster
+    @roster = {}
+  end
+  
+  def add_student(student, grade)
+    if @roster != nil
+      @roster= {grade => [student]}
+    else 
+    student.each {|student| @roster[grade] << student}
+    end
+    
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end 
+  
+
+end 
