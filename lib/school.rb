40c83b9287782a0f21d5cb777586fd6ec019386d
Amee Covarrubias <ameecova@yahoo.com>
2018-07-28 00:31:22 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,13 +9,8 @@ class School
   end
 
   def add_student(student, grade)
-    if @roster == nil 
       @roster[grade] ||= []
       @roster[grade] << student
-    else
-      @roster[grade] << student
-    end 
-    @roster 
   end 
   
   def grade(grade)
